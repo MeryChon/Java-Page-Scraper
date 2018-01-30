d8f7aad52e4c689bd07091abe10f09566d3b5a83
MeryChon <mchon14@freeuni.edu.ge>
2018-01-31 00:03:37 +0400

    Updated code to include possibility of only scraping images(or links)

--- src/jpspackage/PageScraper.java
+++ src/jpspackage/PageScraper.java
@@ -11,34 +11,75 @@ import java.util.Set;
 import java.util.ArrayList;
 
 public class PageScraper {
-	private Set<String> parsedElements;
+	private Set<String> scrapedElements;
 	public final String LINK = "a";   //TODO: const??
-	public final String IMAGE = "image";
+	public final String IMAGE = "img";
 	
 	public PageScraper() {
-		this.parsedElements = new HashSet<String>();
+		this.scrapedElements = new HashSet<String>();
 	}
+	
+	public void ScrapeLinks(Elements scrapedLinksRaw) {
+		for(Element elem: scrapedLinksRaw) {
+			if(this.scrapedElements.contains(elem.attr("href"))) {
+				System.out.println("DUPLICATE");
+			}
+			this.scrapedElements.add(elem.attr("href"));
+		}
+	}
+	
+	public void ScrapeImages(Elements scrapedImagesRaw) {
+		for(Element elem: scrapedImagesRaw) {
+			if(this.scrapedElements.contains(elem.attr("src"))) {
+				System.out.println("DUPLICATE" + elem.attr("src"));
+			}
+			System.out.println("next element " + elem.attr("src"));
+			this.scrapedElements.add(elem.attr("src"));
+		}
+	}	
+	
 	
 	public void ParseURL(String url, ArrayList<String> tagNames) {		
 		Document doc;
-//		for(String s: tagNames) {
-//			System.out.println(s);
-//		}
+		for(String s: tagNames) {
+			System.out.println(s);
+		}
 		try {
-			doc = Jsoup.connect(url).get();
+			doc = Jsoup.connect(url).maxBodySize(10*1024*1024).get();
 			for(String s: tagNames) {
-				Elements elems = doc.getElementsByTag(s);
-				for(Element elem: elems) {
-					this.parsedElements.add(elem.text());
+				switch(s) {
+				case LINK:
+					this.ScrapeLinks(doc.getElementsByTag(LINK));
+					break;
+				case IMAGE:
+					this.ScrapeImages(doc.getElementsByTag(IMAGE));
+					break;
+				default:
+					this.ScrapeLinks(doc.getElementsByTag(LINK));
+					this.ScrapeImages(doc.getElementsByTag(IMAGE));
+					break;
 				}
+				
+		
+//				Elements scrapedElementsRaw = doc.getElementsByTag(s);
+//				doc.getElementsByAttribute("href");
+//				Elements scrapedElementsRaw = doc.getElementsByAttribute("href");
+//				for(Element elem: scrapedElementsRaw) {
+//					this.scrapedElements.add(elem.attr("href"));
+//				}
 			}
-			for(String s: this.parsedElements) {
+			System.out.println(this.scrapedElements.size());
+			for(String s: this.scrapedElements) {
 				System.out.println(s);
 			}
 		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void WriteParsedDataToFile(String fileName) {
+		
 	}
 
 	
--- src/jpspackage/RunPageScraper.java
+++ src/jpspackage/RunPageScraper.java
@@ -8,10 +8,28 @@ public class RunPageScraper {
 		PageScraper ps = new PageScraper();
 		System.out.println("Enter webpage URL and hit enter.");
 		Scanner scin = new Scanner(System.in);  // TODO : fix the warning
+		
 		String webPageName = scin.nextLine();
+		System.out.println("What kind of elements do you want to get?");
+		System.out.println("Enter l for links, i for images and b for both.");
+		String typeOfElements = scin.nextLine();
+		
 		ArrayList<String> tags = new ArrayList<String>();
-		tags.add(ps.LINK);
-		tags.add(ps.IMAGE);
+		switch(typeOfElements) {
+		case "l":
+			tags.add(ps.LINK);
+			break;
+		case "i":
+			tags.add(ps.IMAGE);
+			break;
+		case "b":
+			tags.add(ps.LINK);
+			tags.add(ps.IMAGE);
+			break;
+		default:
+			return;
+		}	
+		
 		ps.ParseURL(webPageName, tags);		
 	}
 }
