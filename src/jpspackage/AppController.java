37d449e0da6ff1ccd7daa53de0c3a7267b840c50
MeryChon <mchon14@freeuni.edu.ge>
2018-02-01 19:37:31 +0400

    Added app controller class, to resemble MVC pattern

---
+++ src/jpspackage/AppController.java
@@ -0,0 +1,26 @@
+package jpspackage;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppController{
+	private PageScraper pScraper;
+	public AppController(PageScraper pageScraper) {
+		this.pScraper = pageScraper;
+	}
+	
+	public void scrapePage(String url, String saveDir, boolean links, boolean images) {
+		String tagType;
+//		if(!links && !images) return;
+		if(links) {
+			if(images) {
+				tagType = PageScraper.LINK_AND_IMAGE;
+			} else {
+				tagType = PageScraper.LINK;
+			}
+		}
+		tagType = PageScraper.IMAGE;
+		this.pScraper.ScrapeURL(url, tagType);
+	}
+
+}
