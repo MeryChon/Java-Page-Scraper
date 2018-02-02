a30ba4f73297ffe1a706ec6e6d44814c4b057669
MeryChon <mchon14@freeuni.edu.ge>
2018-02-02 10:56:27 +0400

    created error window class

---
+++ src/jpspackage/ErrorWindow.java
@@ -0,0 +1,70 @@
+package jpspackage;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import javax.swing.UIManager;
+
+public class ErrorWindow extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		try {
+//			ErrorWindow dialog = new ErrorWindow();
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ErrorWindow(String message) {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JTextArea messageArea = new JTextArea();
+		messageArea.setBackground(UIManager.getColor("Button.background"));
+		messageArea.setLineWrap(true);
+		messageArea.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		messageArea.setRows(3);
+		messageArea.setBounds(10, 40, 415, 115);
+		messageArea.setEditable(false);
+		messageArea.setText(message);
+		contentPanel.add(messageArea);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent arg0) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+}
